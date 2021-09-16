e0c71204a6ac455c6ec7449f0fff5bb37d1ab50e
Achraf Essaoudi <achrafess1937@gmail.com>
2021-09-16 12:39:26 +0100

    change

--- an-test.js
+++ an-test.js
@@ -2,10 +2,7 @@ let seconds = 2;
 let minutes = 50;
 let hours = 03;
 let days = 8;
-let flipped = true;
-let flipped2 = true;
-let flipped3 = true;
-let flipped4 = true;
+let flipped = [true, true, true, true];
 
 let dayszero = false;
 let hourszero = false;
@@ -24,18 +21,30 @@ let hour2 = document.getElementById("hour-2");
 let day1 = document.getElementById("day-1");
 let day2 = document.getElementById("day-2");
 
+//seconds
 setInterval(function () {
   secondsFunction();
 }, 1000);
 setTimeout(function () {
-  hello();
+  minutesIn();
   minutesFunction();
 }, 1000 * 61 - (60 * 1000 - seconds * 1000));
 
-function hello() {
+//////////////////////
+function minutesIn() {
   setInterval(function () {
     minutesFunction();
   }, 60 * 1000);
+}
+setTimeout(function () {
+  HoursIn();
+  hoursFunction();
+}, 3.6e6 - (60 * 1000 - seconds * 1000) - (3.6e6 - hours * 1000));
+
+function hoursIn() {
+  setInterval(function () {
+    hoursFunction();
+  }, 2.16e8);
 }
 sec1.innerHTML = seconds;
 sec2.innerHTML = seconds;
@@ -54,77 +63,70 @@ function secondsFunction() {
     seconds = 60;
     // minutesFunction();
   }
-  if (flipped === true) {
+  if (flipped[0] === true) {
     seconds -= 1;
     seconds.toString().length == 2
       ? (sec2.innerHTML = seconds)
       : (sec2.innerHTML = 0 + `${seconds}`);
-    flipped = false;
+    flipped[0] = false;
   } else {
     seconds -= 1;
     seconds.toString().length == 2
       ? (sec1.innerHTML = seconds)
       : (sec1.innerHTML = 0 + `${seconds}`);
-    flipped = true;
+    flipped[0] = true;
   }
 }
 
 function minutesFunction() {
-  if (hourszero != true) {
-    cardmin.classList.toggle("trans");
+  cardmin.classList.toggle("trans");
 
-    if (minutes == 0) {
-      minutes = 60;
-      hoursFunction();
-    }
-    if (flipped2 === true) {
-      minutes -= 1;
-      minutes.toString().length == 2
-        ? (min2.innerHTML = minutes)
-        : (min2.innerHTML = 0 + `${minutes}`);
-      flipped2 = false;
-    } else {
-      minutes -= 1;
-      minutes.toString().length == 2
-        ? (min1.innerHTML = minutes)
-        : (min1.innerHTML = 0 + `${minutes}`);
-      flipped2 = true;
-    }
+  if (minutes == 0) {
+    minutes = 60;
+    hoursFunction();
+  }
+  if (flipped[1] === true) {
+    minutes -= 1;
+    minutes.toString().length == 2
+      ? (min2.innerHTML = minutes)
+      : (min2.innerHTML = 0 + `${minutes}`);
+    flipped[1] = false;
+  } else {
+    minutes -= 1;
+    minutes.toString().length == 2
+      ? (min1.innerHTML = minutes)
+      : (min1.innerHTML = 0 + `${minutes}`);
+    flipped[1] = true;
   }
 }
 
 function hoursFunction() {
-  if (days !== 0) {
-    cardhour.classList.toggle("trans");
-    if (hours == 0) {
-      hours = 24;
-      daysFunction();
+  cardhour.classList.toggle("trans");
+  if (hours == 0) {
+    hours = 24;
+    daysFunction();
+  } else {
+    if (flipped[2] === true) {
+      hours -= 1;
+      hour2.innerHTML = hours;
+      flipped[2] = false;
     } else {
-      if (flipped3 === true) {
-        hours -= 1;
-        hour2.innerHTML = hours;
-        flipped3 = false;
-      } else {
-        hours -= 1;
-        hour1.innerHTML = hours;
-        flipped3 = true;
-      }
+      hours -= 1;
+      hour1.innerHTML = hours;
+      flipped[2] = true;
     }
   }
 }
+
 function daysFunction() {
   carddays.classList.toggle("trans");
-  if (days == 0) {
-    days = 0;
+  if (flipped[3] === true) {
+    days -= 1;
+    day2.innerHTML = days;
+    flipped[3] = false;
   } else {
-    if (flipped4 === true) {
-      days -= 1;
-      day2.innerHTML = days;
-      flipped4 = false;
-    } else {
-      days -= 1;
-      day1.innerHTML = days;
-      flipped4 = true;
-    }
+    days -= 1;
+    day1.innerHTML = days;
+    flipped[3] = true;
   }
 }
